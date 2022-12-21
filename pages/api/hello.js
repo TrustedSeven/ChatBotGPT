bb6fcc718d342a13e08351282dbc1fc87af4bc36
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2022-12-20 20:09:13 -0600

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const openai = require('openai');
+openai.apiKey = "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";
+openai.completions({
+         engine: "text-davinci-003",
+                   prompt: "Hello, how are you?",
+                   max_tokens: 32,
+                   n: 1,
+stop: ".",
+                   temperature: 0.5,
+              }).then((response) => {
+console.log(response.data.choices[0].text);
+});
